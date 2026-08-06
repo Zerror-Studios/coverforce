1577a06763a9f83375dd811b20f28148832c45c3
Ayush-ZR <ayush.zerror@gmail.com>
2026-08-06 18:33:45 +0530

    remove webflow carrier api

--- src/app/integration/page.tsx
+++ src/app/integration/page.tsx
@@ -5,19 +5,17 @@ import CardSection from "@/components/integration/CardSection";
 import IntegrationStats from "@/components/integration/IntegrationStats";
 import Integration from "@/components/integration/Integration";
 import { createPageMetadata } from "@/lib/seo";
-import { getWebflowCarriers } from "@/lib/webflow";
 
 export const metadata = createPageMetadata("/integration");
 export const revalidate = 3600;
 
 const IntegrationPage = async () => {
-  const carriers = await getWebflowCarriers();
   return (
     <PageWrapper>
       <Hero />
       <CardSection />
       <IntegrationStats />
-      <Integration initialCarriers={carriers} />
+      <Integration/>
     </PageWrapper>
   );
 };
