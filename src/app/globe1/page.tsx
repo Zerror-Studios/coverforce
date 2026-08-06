7ec7cee0cbdec19fe6590ff783e32b11be565351
sunny-zerror <sunny.zerror@gmail.com>
2026-08-06 15:35:08 +0530

    new globe added

---
+++ src/app/globe1/page.tsx
@@ -0,0 +1,14 @@
+import RequestGlobe from '@/components/home/Globe/RequestGlobe'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <div className="w-full h-screen pt-20  flex items-center justify-center">
+            <RequestGlobe/>
+        </div>
+    </>
+  )
+}
+
+export default page
